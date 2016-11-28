79e50d7ed7c4ebc58e93bf623709ed5d995ff418
Andrey Chernih <andrey.chernih@gmail.com>
2016-11-28 13:04:26 -0800

    Add basic leaderboard

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -3,12 +3,16 @@
 angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
+  $routeProvider.when('/leaderboard', {
     templateUrl: 'view2/view2.html',
     controller: 'View2Ctrl'
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope', function($scope) {
+  $scope.scores = [
+    { 'zip_code': '94040', 'score': 20, 'rank': 1 },
+    { 'zip_code': '94050', 'score': 10, 'rank': 2 },
+    { 'zip_code': '94060', 'score': 5,  'rank': 3 },
+  ];
 }]);
