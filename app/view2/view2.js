92f8d26b89d1f98fde41e2f056f4d5bced32ff33
Andrey Chernih <andrey.chernih@gmail.com>
2016-11-28 18:01:45 -0800

    Get user post code

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,7 +9,26 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', ['$scope', function($scope) {
+.controller('View2Ctrl', ['$scope', 'currentUserService', function($scope, currentUserService) {
+  var user = currentUserService.get();
+  var zipCode;
+  if (user) {
+    user.getUserAttributes(function(err, result) {
+      if (err) {
+        alert(err);
+        return;
+      }
+      for (var i = 0; i < result.length; i++) {
+        if (result[i].getName() == 'custom:zip_code') {
+          zipCode = result[i].getValue();
+          break;
+        }
+      }
+
+      console.log(zipCode);
+    });
+  }
+
   $scope.scores = [
     { 'zip_code': '94040', 'score': 20, 'rank': 1 },
     { 'zip_code': '94050', 'score': 10, 'rank': 2 },
