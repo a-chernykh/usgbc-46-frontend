be473bfc1835069153ac233e4acf9d8cb0aed8e8
Aziz Bodal <Aziz@imo-online.com>
2016-11-28 20:18:28 -0600

    Connected to gateway

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,10 +9,17 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', ['$scope', function($scope) {
+.controller('View2Ctrl', ['$scope', '$http', function($scope, $http) {
+  //https://h5c128n3tb.execute-api.us-west-2.amazonaws.com/dev/leaderboard?lat=123&lon=234&Test=2
+  $http.get("https://h5c128n3tb.execute-api.us-west-2.amazonaws.com/dev/leaderboard?lat=123&lon=234&Test=2")
+    .then(function(response) {
+        $scope.scores = response.data.scores;
+    });
+/*
   $scope.scores = [
     { 'zip_code': '94040', 'score': 20, 'rank': 1 },
     { 'zip_code': '94050', 'score': 10, 'rank': 2 },
     { 'zip_code': '94060', 'score': 5,  'rank': 3 },
   ];
+*/
 }]);
